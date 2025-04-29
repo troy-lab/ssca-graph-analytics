e351fdcd93937cfc00e3b2228ea55bf5955b61d1
Troy timmerman <ttimmer2@jhu.edu>
2025-04-29 18:49:01 -0500

    adding latest programming work

--- ssca/ssca.py
+++ ssca/ssca.py
@@ -1,6 +1,9 @@
+import random
 from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
 import numpy as np
 from sklearn.metrics import pairwise_distances
+from scipy import linalg
 
 
 # Step 1 functions - construct similarity matrix.
@@ -52,7 +55,7 @@ def calculate_similarity_matrix(dataset, k):
 def update_pairwise_constraints(W, must_link, cannot_link):
     """
     This step adds the link constraints to the similarity matrix
-    :param W: similarity matrix
+    :param W: Similarity matrix
     :param must_link: required links (same label)
     :param cannot_link: cannot link (different label)
     :return:
@@ -60,11 +63,67 @@ def update_pairwise_constraints(W, must_link, cannot_link):
     for point in must_link:
         i = point[0]
         j = point[1]
-        W[i,j] = 0
-        W[j,i] = 0
+        W[i,j] = 1
+        W[j,i] = 1
     for point in cannot_link:
         i = point[0]
         j = point[1]
-        W[i,j] = 1e6
-        W[j,i] = 1e6
+        W[i,j] = 0.001
+        W[j,i] = 0.001
     return W
+
+
+def construct_L(W):
+    """
+    This constructs L which we will use to calculate our eigenvalues
+    :param W:
+    :return:
+    """
+    d = W.sum(axis=1)
+    D_inv_sqrt = np.diag(1 / np.sqrt(d))
+    L = D_inv_sqrt.dot(W).dot(D_inv_sqrt)
+    return L
+
+
+def get_eig(matrix):
+    """
+    This is step 3. Calculate unit orthogonal eigenvectors and eigenvalues
+    :param matrix: L
+    :return: tuple[array, array]
+    """
+    eigenvalues, eigenvectors  = linalg.eigh(matrix)
+    return eigenvalues, eigenvectors
+
+
+def sample_links(num_links, labels):
+    must_link = []
+    cannot_link = []
+    for l in list(set(labels)):
+        indices = list(np.where(labels == l)[0])
+        for _ in range(num_links):
+            a, b = random.sample(indices, 2)
+            must_link.append((int(a), int(b),))
+            not_indices = list(np.where(labels != l)[0])
+            a = random.sample(not_indices, 1)[0]
+            b = random.sample(indices, 1)[0]
+            cannot_link.append((int(a), int(b),))
+    return must_link, cannot_link
+
+def ssca(X, y, must_link, cannot_link):
+    k = len(list(set(y)))
+    W = calculate_similarity_matrix(X, k)
+    W = update_pairwise_constraints(W, must_link, cannot_link)
+    assert np.allclose(W, W.T)
+    L = construct_L(W)
+    eigenvalues, eigenvectors = get_eig(L)
+    idx = np.argsort(eigenvalues)[-k:]
+    idx = idx[np.argsort(eigenvalues[idx])[::-1]]
+    X = eigenvectors[:, idx]
+    norms = np.linalg.norm(X, axis=1, keepdims=True)   # shape (n,1)
+    Y = X / norms
+    km = KMeans(n_clusters=k, init='k-means++', n_init=10, max_iter=100,random_state=0)
+    km.fit(Y)
+    y_hat = km.labels_
+    return y_hat
+
+
